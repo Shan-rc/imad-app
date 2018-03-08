26adf75425e3c58edf781a4dbc3f609439efc20e
Shan-rc <36488755+Shan-rc@users.noreply.github.com>
2018-03-09 02:36:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('Counter');
+var counter = 0;
+
+button.oneclick = function () {
+  
+  // make a request to the counter end point
+  
+  // capture the response and store it in a variable
+   
+  // Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('Count');
+  span.innerHTML = counter.toString();
+  };
